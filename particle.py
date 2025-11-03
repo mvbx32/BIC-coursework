49eb5040f2060870da2e02f5784e11b80d7c17bb
Maxime Vieillot <maxime.vieilot@yahoo.com>
2025-11-03 01:26:05 +0000

    Fitness and Informants functions as Global parameters of the class Particle. Debbugging.

--- particle.py
+++ particle.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random 
-from ANN import ANN
+from ANN_alone import ANN
 # Clean the code recursively
 
 class Particle():
@@ -14,71 +14,84 @@ class Particle():
     """
     # == Common variables == 
 
-    ANN_struture = None
+    ANN_structure = None
+    AssessFitness = callable
+    Informants    = callable
     
-    # x!
+    # -- X! -- 
     fittest_solution = None
     best_fitness = -1*np.inf     
     
-    def __init__(self, params, vel):  # To test # To do : give a default random params value
+    def __init__(self):  # To test # To do : give a default random params value
     
         # * Error * 
-        assert(Particle.ANN_struture == None) # -- Error Msg : ANN_structure undefined --
-        
+        assert(Particle.ANN_structure != None) # -- Error Msg : ANN_structure undefined --
+        assert(Particle.AssessFitness != callable) # -- Error Msg : Fitness undefined --
+        assert(Particle.Informants != callable)    # -- Error Msg : Informants selection undefined --
+
+        # Construction of the vector representation
     
-        # Find the corresponding vector representation
-        # Boucle FOR : parcours structure layer par layer, neuron par neuro => poids initialisé ALEATOIRE SI params = NONE 
-        
-        self.vector = []
+        weight_list = []
 
         for i in range(1,len(Particle.ANN_structure)):
             # for each layer
 
-            for neur_i in range(Particle.ANN_struture[i]):  
+            for neur_i in range(Particle.ANN_structure[i]):  
                 # for each neuron
 
-                for wi in range(Particle.ANN_struture[i-1]) :
+                for wi in range(Particle.ANN_structure[i-1]) :
                     # for each weigth to the neuron  
-                    self.vector.append(random.random())
+                    weight_list.append(random.random())
 
-        self.vector = np.array(self.vector).transpose()
-
-
-
-             
-
-        self.vector = None  #self.get_parameters_vector(self.ANN_structure)
-        self.velocity = 0
+        self._vector = np.array(weight_list).transpose()
+        self.velocity = np.zeros_like(self.vector)
 
         # Instantiation of the ANN used to compute the fitness
-        self.ANN_model = ANN(layer_sizes = Particle.ANN_struture) 
-        
-
-
-
-        # x*
-        self.fitness = np.inf
-        self._best_fitness = np.inf
-
-        # == PSO variables == 
-        self._informants = None # list of Particle Type 
-
-        # TODO : If we are at t, and looking into xi "previous best ..." could it be xi_1(t) ????
-        self.best_x =  self.__vector  
-        self.best_informant = None # set as self.x_informant.xbest 
-        if Particle.fittest_solution == None  : Particle.fittest_solution = self.__vector
-
-
+        self.ANN_model = ANN(layer_sizes = Particle.ANN_structure, activations=["relu","relu"]) 
     
-    def ANN_to_vector(ANN) : # TODO : complete # To test # Remove 
-        return None
+        
+        
+        # -- X* --
+        self._best_fitness = -1*np.inf # integrate the FItness function to the class sinon, initialisant à np.inf on peut passer à côté de la solution
+        self.best_x =  self.vector  
+
+        self._informants = [self] # list of Particle Type 
+
+        # ??? Previous
+        
+        # -- X+ -- 
+        self.best_informant =  self  # set as self.x_informant.xbest 
+        if type(Particle.fittest_solution) != np.array  : Particle.fittest_solution = self.vector
+
+        self.fitness = Particle.AssessFitness(self)
 
     def __eq__(self, other): 
-        if self.vector == other.vector :
+        if not((self.vector - other.vector).all()) :
             return True 
         return False
+    
+    # == Vector == 
+    @property
+    def vector(self) :
+        return self._vector
+    
+    @vector.setter
+    def vector(self,v):
+        self.vector = v
+        # preparation of the ANN model for the fitness calculus
+        self.ANN_model = self.vector2ANN()
+    
 
-     
+    # ==   ANN     == 
+    def vector2ANN(self):
+        i = 0
+        for k in range(1,len(self.ANN.layers)) :                      # for each layer
+            for l in range(len(self.ANN.layers[i])):                    # for each neuron of the layer
+                for j in range(len(self.ANN.layers[i-1])) :               # for each input of the neuron
+                    self.ANN.layers[i].W[j,l]= self.vector[i]
+                    i+=1
+
+        
     # == Fitness == 
     @property
     def fitness(self): # To test
@@ -86,15 +99,25 @@ class Particle():
     
     @fitness.setter
     def fitness(self, new_fitness):  # To test
+        
+        self.__fitness = new_fitness
+
+        # x*
         if self._best_fitness < new_fitness : 
             self._best_fitness =  new_fitness 
             self.best_x = self.vector
 
+            # x+
+            if new_fitness > self.best_informant.fitness : 
+                self.best_informant = self
+
+            # x!
             if new_fitness > Particle.best_fitness : 
                 Particle.best_fitness = new_fitness
                 Particle.fittest_solution = self.vector
-            
-        self.__fitness = new_fitness
+
+    def assessFitness(self):
+        self.fitness= Particle.AssessFitness(self)
 
     @property
     def informants(self): # To test
@@ -110,10 +133,10 @@ class Particle():
     
         for infor in new_informants : 
             if infor.fitness > self.best_informant.fitness : 
-                self.best_informant.fitness  = infor
+                self.best_informant = infor
 
 
-    def export(self):
+    def export(self): #TODO : complete
         """
         Export the vector as a .txt file. 
         """
@@ -122,5 +145,6 @@ class Particle():
     
 
 if __name__ == "__main__":
+    Particle.ANN_structure = [8,5,1]
     Particle()
 
