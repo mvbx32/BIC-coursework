8cada6058281f5a212d9c13444cf473ca34ac668
Maxime Vieillot <maxime.vieilot@yahoo.com>
2025-11-18 00:34:11 +0000

    Ploting

--- main.py
+++ main.py
@@ -32,15 +32,15 @@ import argparse
 
 DEFAULTS = { 
     "exp" : "",
-    "swarmsize": 20,
+    "swarmsize": 5,
     "alpha": 0.9,
     "beta": 1.25,
     "gamma": 1,
     "delta": 1,
     "epsi": 0.5,
     "informants_number": 5,
-    "max_iteration_number": 100,
-    "AttemptNumber": 1,
+    "max_iteration_number": 10,
+    "AttemptNumber": 3,
     "ANN": [8, "input", 5, "sigmoid", 1, "linear"],
     "IDE": True,     # <-- default: use the parameters from code
 }
@@ -150,10 +150,24 @@ if __name__ == "__main__" :
                             "Evaluation of the impact of the number of iteration",
                             variables_of_interest)
     # -----------                                                                ---------------- #
+
+
     
+
     pso_id = 0 
+
+    
+
     for iter in max_iteration_numberList : 
-        for swarmsize in swarmsizeList : 
+        PSO_number = len(swarmsizeList) * AttemptNumber
+        PSOsGlobalVelMeanComponents = np.zeros((4,iter,PSO_number))
+        PSOsGLobalDistance = np.zeros((3,iter,PSO_number))
+        PSOsBestImprov = np.zeros((PSO_number,iter))
+      
+        PSOsBestsId = [] #np.zeros((PSO_number,iter))
+
+        step = 0
+        for s, swarmsize in enumerate(swarmsizeList) : 
             
             # --  Monitoring ------------------------------------------------------
             print("pso id {} itermax {} swarm {}  ".format(pso_id, iter, swarmsize))
@@ -165,7 +179,7 @@ if __name__ == "__main__" :
             Time =  []
 
             
-            for attempt in range(AttemptNumber):
+            for ai, attempt in enumerate(range(AttemptNumber)):
 
                 # $ - - For Debugging only - - $
                 #np.random.seed(42)
@@ -178,9 +192,6 @@ if __name__ == "__main__" :
                 if iter == max(max_iteration_numberList) and attempt == AttemptNumber-1 :
                     verbose = 0
 
-                if attempt in [5] : 
-                    print("Attempt ", attempt)
-                
                 pso= PSO(swarmsize, 
                             alpha, 
                             beta, 
@@ -192,7 +203,7 @@ if __name__ == "__main__" :
                             informants_number, 
                             Informants, 
                             max_iteration_number = iter, 
-                            verbose = verbose) 
+                            verbose = verbose, path = "/experiments/" + experiment_name, show = False ) 
                 
                 
                 # ////////////// Params to increment //////////////////////
@@ -200,6 +211,7 @@ if __name__ == "__main__" :
                 pso.swarmsize = swarmsize
                 #  == Results ==============================================================
                 best_solution, best_fitness, score_train, score_test, run_time = pso.train()  
+                
                 
 
                 #___________________________________________________________________________
@@ -211,6 +223,9 @@ if __name__ == "__main__" :
                 Train.append(score_train)
                 Test.append(score_test)
                 Time.append(run_time)
+
+
+
                 if attempt == 0 :
                     params = { 
                                 "ANNStructure" :  str(ANNStructure) , "swarmsize": str(swarmsizeList),"AssessFitness" : str(AssessFitness.__doc__), "max_iteration_number" : str(max_iteration_numberList),
@@ -222,32 +237,121 @@ if __name__ == "__main__" :
                     save_pso_params(pso_dir, params)
                 # -----------                                -------------                             ---------------- #
 
+                # --  Exploitation  ----------------------------------------------------------------------------------------- #
 
-            # --  Exploitation  ----------------------------------------------------------------------------------------- #
+                fitness_avg, score_train_avg,score_train_std,score_test_avg, score_test_std, time_avg =np.mean(Fitness), np.mean(Train),np.std(Train),np.mean(Test),np.std(Test),np.mean(Time)
+                print(fitness_avg, score_train_avg,score_train_std,score_test_avg, score_test_std, time_avg)
+                
+                PSOsGlobalVelMeanComponents[:,:,step] = pso.globalVelMeanComponents.copy()
+                PSOsGLobalDistance[:,:,step]  = np.array([pso.MinDistance, pso.AVGDistance, pso.MaxDistance])
+                PSOsBestsId.append(pso.BestPaternityHistory)
+                PSOsBestImprov[step,:] = pso.ImprovementOfBest[1:]
+                # --  Export        ----------------------------------------------------------------------------------------- #
+                
+                row_data = [pso_id,fitness_avg, 
+                            score_train_avg,
+                            score_train_std, 
+                            score_test_avg,
+                            score_test_std,
+                            AttemptNumber,
+                            pso.max_iteration_number,
+                            run_time,
+                            pso.swarmsize,
+                            pso.informants_number,
+                            alpha,beta,gamma,delta,epsi,
+                            str(pso.ANN_structure).replace('[','').replace(']','').replace(',',' ')]
+                row_data_str = [str(data) for data in row_data if (type(data)!= float) or (type(data)!= int)]
+                
+                append_results_to_excel(results_path, row_data)
+                print(s + ai, step)
+                step +=1
+                # == END of the attempts == #
 
-            fitness_avg, score_train_avg,score_train_std,score_test_avg, score_test_std, time_avg =np.mean(Fitness), np.mean(Train),np.std(Train),np.mean(Test),np.std(Test),np.mean(Time)
-            print(fitness_avg, score_train_avg,score_train_std,score_test_avg, score_test_std, time_avg)
-            
-            # --  Export        ----------------------------------------------------------------------------------------- #
-            
-            row_data = [pso_id,fitness_avg, 
-                        score_train_avg,
-                        score_train_std, 
-                        score_test_avg,
-                        score_test_std,
-                        AttemptNumber,
-                        pso.max_iteration_number,
-                        run_time,
-                        pso.swarmsize,
-                        pso.informants_number,
-                        alpha,beta,gamma,delta,epsi,
-                        str(pso.ANN_structure).replace('[','').replace(']','').replace(',',' ')]
-            row_data_str = [str(data) for data in row_data if (type(data)!= float) or (type(data)!= int)]
-            
-            append_results_to_excel(results_path, row_data)
 
-            # == END of the attempts == #
 
+        PSOsGlobalVelMeanComponentsAVG = np.mean(PSOsGlobalVelMeanComponents, axis = 2)
+        PSOsGlobalVelMeanComponentsMAX = np.max(PSOsGlobalVelMeanComponents, axis = 2)
+        PSOsGlobalVelMeanComponentsMIN = np.min(PSOsGlobalVelMeanComponents, axis = 2)
+        
+        PSOsBestImprovAVG = np.mean(PSOsBestImprov,axis = 0)
+        PSOsBestImprovSTD = np.std(PSOsBestImprov,axis = 0)
+        plt.rcParams["figure.figsize"] = (10, 6)
+        plt.rcParams["figure.dpi"] = 120
+        plt.rcParams["savefig.dpi"] = 150
+        
+
+         # === 1 — Relative improvements of the SWARM ===
+        fig0, ax0 = plt.subplots()
+        ax0.set_title("Relative improvements of the Bests")
+        #ax0.set_ylim([-0.5, 0.5])
+        ax0.set_xlabel("iteration")
+        ax0.set_ylabel("relative improvement")
+
+        iterations = range(max_iteration_number)
+
+        ax0.plot(iterations,PSOsBestImprovAVG, 'o', c="#008fd5", label="Average")
+        ax0.fill_between(iterations,
+                        np.array(PSOsBestImprovAVG)-np.array(PSOsBestImprovSTD),
+                        np.array(PSOsBestImprovAVG)+np.array(PSOsBestImprovSTD),
+                        color="#BBE4F8",
+                        label="Standard deviation")
+        for ps in range(PSO_number) : 
+            line = ax0.plot(iterations,PSOsBestImprov[ps,:], '.')[0]
+            ax0.plot(iterations,PSOsBestImprov[ps,:], linestyle = 'solid',linewidth = 0.3,color =  line.get_color())
+        ax0.legend(loc="best")
+        fig0.tight_layout(pad=2)
+        fig0.savefig( "Relative improvements of the Bests.png", bbox_inches='tight')
+
+        # === 3 — Interparticle distances ===
+        fig1, ax1 = plt.subplots()
+        ax1.set_title("Interparticle distances")
+        ax1.set_xlabel("iteration")
+        ax1.set_ylabel("distance")
+        ax1.plot(range(iter),np.max(PSOsGLobalDistance, axis = 2)[2,:] , label="max", linewidth= 0.8)
+        ax1.plot(range(iter),np.mean(PSOsGLobalDistance, axis = 2)[0,:], label="average", linewidth= 0.8)
+        ax1.plot(range(iter),np.min(PSOsGLobalDistance, axis = 2)[0,:], label="min", linewidth= 0.8)
+        ax1.legend(loc="best")
+
+        fig1.tight_layout(pad=2)
+        fig1.savefig("Interparticle_distances.png", bbox_inches='tight')
+
+        fig2, ax2 = plt.subplots(4,1)
+
+        for c, component in enumerate(["inertial","local","social","global" ]) :
+            ax2[c].title.set_fontsize(0.25 + 2)
+            ax2[c].xaxis.label.set_fontsize(0.25)
+            ax2[c].yaxis.label.set_fontsize(0.25)
+            ax2[c].set_title("Mean(|{} velocity |)".format(component))
+            ax2[c].plot(range(iter), PSOsGlobalVelMeanComponentsAVG[c,:], label="MEAN", linewidth=0.8)
+            ax2[c].plot(range(iter), PSOsGlobalVelMeanComponentsMIN[c,:], label="MIN", linewidth=0.8)
+            ax2[c].plot(range(iter), PSOsGlobalVelMeanComponentsMAX[c,:], label="MAX", linewidth=0.8)
+        ax2[c].set_xlabel("iteration")
+        ax2[c].legend(loc="best")
+        fig2.tight_layout(pad=2)
+        # --- SAVE IN THE EXPERIMENT FOLDER ---
+        fig2.savefig("PSOSVelocity_components.png",
+                    bbox_inches='tight')
+
+        
+
+
+         # === 5 — Best particle ID ===
+        fig3, ax3 = plt.subplots()
+        ax3.set_title("Id of the best particle")
+        ax3.set_ylim([0, swarmsize + 1])
+        ax3.set_xlabel("iteration")
+        ax3.set_ylabel("id")
+
+        for ps in range(PSO_number):
+            line = ax3.plot(range(iter),PSOsBestsId[ps], "s")
+            #ax3.plot(range(iter),PSOsBestsId[ps], linestyle ="solid", linewidth = 0.8, c = line[0].get_color())
+            ax3.grid(True)
+            ax3.set_yticks([i+1 for i in range(swarmsize)])
+
+        fig3.tight_layout(pad=2)
+        fig3.savefig("Best_particle_id.png", bbox_inches='tight')
+
+        plt.show()
         # == END of the evaluations  == #
 
 
